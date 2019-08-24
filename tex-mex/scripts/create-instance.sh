db4d4de3c44bb786d020df340d1f0314b42d7d14
Kenny Goodin <kennethbgoodin@gmail.com>
2019-08-23 20:54:07 -0400

    Add scripts for starting an instance

---
+++ tex-mex/scripts/create-instance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# start instance
+aws ec2 run-instances --image-id ami-095dfab8d6fbf28d9 --key-name grover-tex-mex --security-groups tex-mex-ssh tex-mex-http tex-mex-http-for-docker-images --instance-type g3s.xlarge --placement AvailabilityZone=us-east-2a --block-device-mappings DeviceName=/dev/sdh,Ebs={VolumeSize=100} --count 1 --user-data file://cloud-init.sh
+
+echo "waiting 30 seconds for instance to start..."
+sleep 30
+
+ip=$(aws ec2 describe-instances --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PublicIpAddress' | sed 's/[][]//g' | tr -d ' "\t\n\r\f')
+
+echo "got ip $ip, writing to running_instance_ip.txt"
+echo "$ip" > running_instance_ip.txt
