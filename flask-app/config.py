11e8e845cab1aba03e7fea292062ef15d6147631
Conrad <cdstosz@gmail.com>
2019-08-25 14:39:31 -0400

    Add contact-us page.

--- flask-app/config.py
+++ flask-app/config.py
@@ -1,7 +1,12 @@
-port = 80
+port = 5000
 
 # False to disable article generation, such as for testing.
-generate_new_articles = True
+generate_new_articles = False
 
 google_search_api_key = ""
 google_custom_search_engine_id = ""
+
+# telegram bot API token for sending notifications from "contact-us" page
+telegram_bot_token = ""
+# telegram bot's chat id to send messages to
+telegram_bot_chatid = ""
