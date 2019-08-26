1a4f2a7efea8639f8a0d1df55dadf2d7b1a4fb85
Kenny Goodin <kennethbgoodin@gmail.com>
2019-08-25 21:54:43 -0400

    Output ip to file

--- tex-mex/scripts/create-instance.sh
+++ tex-mex/scripts/create-instance.sh
@@ -4,5 +4,6 @@
 aws ec2 run-instances --image-id ami-058af1402f6c2e8fa --key-name grover-tex-mex --security-groups tex-mex-ssh tex-mex-http tex-mex-http-for-docker-images --instance-type g3s.xlarge --placement AvailabilityZone=us-east-2a --count 1 --instance-initiated-shutdown-behavior terminate --user-data file://cloud-init.sh >/dev/null && \
 sleep 30 && \
 ip=$(aws ec2 describe-instances --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PublicIpAddress' | sed 's/[][]//g' | tr -d ' "\t\n\r\f') && \
-# send ip to stdout
-echo "$ip"
+# send ip to stdoutt & write to file
+echo "$ip" && \
+echo "$ip" >> running_instance_ip.txt
