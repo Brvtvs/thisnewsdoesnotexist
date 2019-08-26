d2038404d07bf377c411b7200c1c65a127f10565
Kenny Goodin <kennethbgoodin@gmail.com>
2019-08-25 21:51:19 -0400

    Remove unused ip grab, run container with nvidia docker

--- tex-mex/scripts/cloud-init.sh
+++ tex-mex/scripts/cloud-init.sh
@@ -2,13 +2,12 @@
 
 # log output in easy to find places
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+service docker status
 # start docker
 service docker start
 # add our user to docker
 usermod -a -G docker ec2-user
-# very professional method of getting public ip
-addr=$(curl ipinfo.io/ip)
-# run our container
-docker run -d -it --name tex-mex -p 8000:8000 --mount type=bind,source=/home/ec2-user/mega,target=/tex-mex/grover/models/mega 244523355081.dkr.ecr.us-east-2.amazonaws.com/tex-mex-grover:latest
-# shutdown 
-echo "shutdown -h" | at now + 55 min
+# run our container with nvidia-docker for gpu support
+nvidia-docker run -d -it --name tex-mex --runtime=nvidia -p 8000:8000 --mount type=bind,source=/home/ec2-user/mega,target=/tex-mex/grover/models/mega 244523355081.dkr.ecr.us-east-2.amazonaws.com/tex-mex-grover:latest
+# self destruct
+echo "sudo shutdown -h" | at now + 55 min
