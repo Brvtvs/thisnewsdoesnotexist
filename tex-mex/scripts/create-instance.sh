2926211bb412bcc37c4a84c7f35a1ae9db0652d8
Kenny Goodin <kennethbgoodin@gmail.com>
2019-08-26 20:50:46 -0400

    Update ami, try different method of self-terminating

--- tex-mex/scripts/create-instance.sh
+++ tex-mex/scripts/create-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start instance
-aws ec2 run-instances --image-id ami-058af1402f6c2e8fa --key-name grover-tex-mex --security-groups tex-mex-ssh tex-mex-http tex-mex-http-for-docker-images --instance-type g3s.xlarge --placement AvailabilityZone=us-east-2a --count 1 --instance-initiated-shutdown-behavior terminate --user-data file://cloud-init.sh >/dev/null && \
+aws ec2 run-instances --image-id ami-097b5b85aa90c2802 --key-name grover-tex-mex --security-groups tex-mex-ssh tex-mex-http tex-mex-http-for-docker-images --instance-type g3s.xlarge --count 1 --instance-initiated-shutdown-behavior terminate --user-data file://cloud-init.sh >/dev/null && \
 sleep 30 && \
 ip=$(aws ec2 describe-instances --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PublicIpAddress' | sed 's/[][]//g' | tr -d ' "\t\n\r\f') && \
 # send ip to stdoutt & write to file
